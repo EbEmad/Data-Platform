6312b448838b5865ffbf82769380ed34288f56e6
Ebrahim Emad <ebemad8@gmail.com>
2026-05-28 18:59:25 +0300

    udate arc

---
+++ install_airflow.sh
@@ -0,0 +1,42 @@
+# Create or replace a kind cluster
+# kind delete cluster --name kind
+kind create cluster --image kindest/node:v1.29.4
+
+
+# Add airflow to my Helm repo
+helm repo add apache-airflow https://airflow.apache.org
+helm repo update
+helm show values apache-airflow/airflow --version 1.16.0 > chart/values-example.yaml
+
+# Export values for Airflow docker image
+export IMAGE_NAME=my-dags
+export IMAGE_TAG=$(date +%Y%m%d%H%M%S)
+export NAMESPACE=airflow
+export RELEASE_NAME=airflow
+
+
+# Build the image and load it into kind
+docker build --pull --tag $IMAGE_NAME:$IMAGE_TAG -f cicd/Dockerfile .
+kind load docker-image $IMAGE_NAME:$IMAGE_TAG
+
+
+# Create a namespace
+kubectl create namespace $NAMESPACE
+
+
+
+
+
+
+#Apply kubernetes secrets
+kubectl apply -f k8s/secrets/git-secrets.yaml
+
+# Install or upgrade Airflow using Helm (pinning to stable chart 1.16.0 which uses Airflow 2.10.5)
+helm upgrade --install $RELEASE_NAME apache-airflow/airflow \
+    --version 1.16.0 \
+    --namespace $NAMESPACE -f chart/values-override.yaml \
+    --set-string images.airflow.tag="$IMAGE_TAG" \
+    --debug
+
+# Port forward the Webserver
+kubectl port-forward svc/$RELEASE_NAME-webserver 8080:8080 --namespace $NAMESPACE
